f22911ea3049a9d500dd5b8da9aacb7fe535622d
WangPeng <15840615776@163.com>
2018-12-05 21:44:36 +0800

    2-3 ~ 2-4
    2-3 ~ 2-4
    工程目录文件  组件  JSX

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,21 @@
+// ES6 结构赋值
 import React, { Component } from 'react';
+
+// 等价于
+// import React from 'react';
+// const Component = React.Component
+// import { Component } from 'react';
+
+// 还可以
+// import React from 'react';
+// class App extends React.Component {
 
 class App extends Component {
   render() {
+    // JSX
     return (
       <div>
-        hello world
+        hello，Wilson
       </div>
     );
   }
